2546c9fe6b6908f8b02f79bf15a7a0f676722abf
adya <myish1084@gmail.com>
2023-07-05 21:33:09 +0300

    Task 1.1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -40,20 +40,14 @@ void Print2DArray(int[,] array)
 void SortArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
-    {
         for (int j = 0; j < array.GetLength(1); j++)
-        {
             for (int k = 0; k < array.GetLength(1) - 1; k++)
             {
                 if (array[i, k] < array[i, k + 1])
                 {
-                    int temp = array[i, k];
-                    array[i, k] = array[i, k + 1];
-                    array[i, k + 1] = temp;
+                    (array[i, k], array[i, k + 1]) = (array[i, k + 1], array[i, k]);
                 }
             }
-        }
-    }
 }
 
 
